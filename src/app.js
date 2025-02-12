f2d0904792cf26f06e85b400a0f1edd11442126d
Thanh Loi <marcomoi395@gmail.com>
2025-02-12 22:11:21 +0700

    add log to debugging

--- src/app.js
+++ src/app.js
@@ -52,11 +52,4 @@ app.get('/discord/register-commands', async (req, res) => {
     res.send('Register commands successfully');
 });
 
-// Hanling Error
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
-
 module.exports = app;
